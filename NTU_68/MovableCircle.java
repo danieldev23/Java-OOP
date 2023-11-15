5f069c974c4ec68fdbccf5fc65de975a924dd981
Đặng Quốc Huy <85658305+danieldev23@users.noreply.github.com>
2023-11-15 21:31:59 +0700

    NTU Exercise 6.8

---
+++ NTU_68/MovableCircle.java
@@ -0,0 +1,35 @@
+package NTU_68;
+
+public class MovableCircle implements Movable {
+    private MovablePoint center;
+    private int radius;
+
+    public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius) {
+        center = new MovablePoint(x, y, xSpeed, ySpeed);
+        this.radius = radius;
+    }
+
+    @Override
+    public void moveUp() {
+        center.y -= center.ySpeed;
+    }
+
+    @Override
+    public void moveDown() {
+        center.y += center.ySpeed;
+    }
+
+    @Override
+    public void moveLeft() {
+        center.x -= center.xSpeed;
+    }
+
+    @Override
+    public void moveRight() {
+        center.x += center.xSpeed;
+    }
+
+    public String toString() {
+        return "Center: " + center + ", Radius: " + radius;
+    }
+}
